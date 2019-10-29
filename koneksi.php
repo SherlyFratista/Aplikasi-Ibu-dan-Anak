62c3c48efff0e022e6a075348445848b567ebcc7
Ricky <mrickychd.university@gmail.com>
2019-10-29 16:10:09 +0700

    penambahan multilevel user dan koneksi host ke cloud

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,17 @@
 <?php
-   $hostname  = "localhost";
-   $username  = "root";
-   $password  = "";
-   $dbname  = "db_bayi";
+   $hostname  = "sql12.freemysqlhosting.net";
+   $username  = "sql12310023";
+   $password  = "XTTnPXlHu7";
+   $dbname  = "sql12310023";
    $db = new mysqli($hostname, $username, $password, $dbname);
+   
+   function query ($query){
+      global $db;
+      $result = mysqli_query($db,$query);
+      $rows = [];
+      while ( $row = mysqli_fetch_assoc($result)){
+          $rows [] =$row;
+      }
+      return $rows;
+      }
 ?>
