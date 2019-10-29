6df76804683d90b3912c18906c131d9377b7486c
SherlyFratista <48596801+SherlyFratista@users.noreply.github.com>
2019-10-29 19:19:04 +0700

    Feedback

---
+++ Feedback/Feedback.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+     <head>
+        <title> Feedback </title>
+        <link rel="stylesheet" type="text/css" href="Style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     </head>
+
+
+     <body Style ="background-color : white;">   
+    
+        <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+        <img src="logo.png" alt="logo" style="width: 50px;">
+        </a>
+        <div class="d-flex flex-row-reverse bd-highlight">
+            <a class="navbar-text" href="#" style="padding: 20px;" data-toggle="moda1" data-target="#moda1Register">Logout</a>
+            <a class="navbar-text" href="#" style="padding: 20px;" data-toggle="moda1" data-target="#exampleModa1Center">Feedback</a>
+            <a class="navbar-text" href="#" style="padding: 20px;" data-toggle="moda1" data-target="#moda1Register">Bantuan</a>
+            <a class="navbar-text" href="#" style="padding: 20px;" data-toggle="moda1" data-target="#exampleModa1Center">Home</a>
+        </div>
+        </div>
+        </nav>
+
+  <nav>
+  <div  Style ="color : rgb(112, 128, 145); font-size: 20px;">
+  <h1><b><center  Style ="background-color : rgb(100, 149, 237)"><br>
+  Feedback
+  <b></h1></center>
+  </div>
+  </nav>
+
+  <br><br><br>
+
+  <center>
+       <form action="config.php" method="GET">    
+       <main role="main" class="container">
+       <div class="jumbotron">
+       <b><h5>Berikan Feedback Untuk Kami</h5></b>
+       <tr>
+       <td><i><p id=content-s style ="color : black">*Masukkan Feedback</i></p>
+       <textarea cols="100" rows="10" Style ="padding : 1px;">Silahkan isi komentar anda</textarea></td><br>
+       <a class="btn btn-lg btn-primary" href="/docs/4.3/components/navbar/" role="button">Kirim &raquo;</a>
+       </div>
+       </main>
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+       <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+       
+           <footer class="footer mt-auto py-3">
+           <div class="container">
+           <center>
+           <span class="text-muted">2019 All Rights Reserved by Ibu & Anak</span>
+           </div>
+           <center>
+           </footer>
+           <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+           <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script></body>
+       </html>
+       
