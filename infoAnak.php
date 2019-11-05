67b33fb4c766c96686644511d1ad37cee4dec042
Khanza <alkhanza@icloud.com>
2019-11-05 21:18:45 +0700

    read more dan connect db

--- infoAnak.php
+++ infoAnak.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+<?php
+
+$DBhost = "localhost";
+$DBuser = "root";
+$DBname = "bidan";
+$DBpass = "";
+
+$conn = mysqli_connect($DBhost,$DBuser, $DBpass,$DBname);
+
+if (!$conn) {
+  die("Connection failed: " .mysqli_connect_error());
+}
+
+?>
+
 <html>
 <head>
 	<title></title>
@@ -45,7 +60,7 @@
       <div class="card-body">
         <h5 class="card-title">Kebutuhan Nutrisi Bagi Si Anak</h5>
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button type="button" href = "#" class="btn btn-outline-primary">Read More</button>
+        <button type="button"  onclick="location.href='readmoreArticle1.php'" class="btn btn-outline-primary">Read More</button>
         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
     </div>
