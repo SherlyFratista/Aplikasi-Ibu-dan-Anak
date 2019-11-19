303d3fdcffd4720ee3b47cd448928772ec548989
SherlyFratista <48596801+SherlyFratista@users.noreply.github.com>
2019-11-20 00:49:01 +0700

    Progress

--- KirimFeedback.php
+++ KirimFeedback.php
@@ -1,45 +1,23 @@
 <?php
-require_once("../koneksi.php");
-
-$statusFeedback = '';
-$msgClass = '';
-if(isset($_POST['submit'])){
-    // Get the submitted form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $mysqli = ("INSERT INTO users (name,email,message) VALUES ({$id}, '{$name}', '{$email}', '{$message}'");
-    $query = $db->query($mysqli);
-    
-    // Cek apakah ada data yang belum diisi
-    if(!empty($email) && !empty($message) && !empty($name)){
-        
-        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-            $statusFeedback = 'Please enter your valid email.';
-            $msgClassk = 'errordiv';
-        }else{
-            // Pengaturan penerima email dan subjek email
-            $toEmail = '';
-            $htmlContent = '
-                <h4>Name</h4><p>'.$name.'</p>
-                <h4>Email</h4><p>'.$email.'</p>
-                <h4>Feedback/h4><p>'.$message.'</p>';
-            
-            // Header tambahan
-            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
-            
-            // Send email
-            if(mail($toEmail,$htmlContent,$headers)){
-                $statusFeedback = 'Pesan Anda sudah terkirim dengan sukses !';
-                $msgClass = 'succdiv';
-            }else{
-                $statusFeedback = 'Maaf pesan Anda gagal terkirim, silahkan ulangi lagi.';
-                $msgClass = 'errordiv';
-            }
-        }
-    }else{
-        $statusFeedback = 'Harap mengisi semua field data';
-        $msgClass = 'errordiv';
+   require_once("koneksi.php");
+   if(isset($_POST['submit'])){
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+   $sql = "SELECT * FROM users WHERE email = '$email'";
+   $query = $db->query($sql);
+   if(!$email || !$message) {
+    echo "<div align='center'>Masih ada data yang kosong! ";
+    ?> <META HTTP-EQUIV="refresh" CONTENT="3;URL=Feedback.php"> <?php
+  } else {
+       $data = "INSERT INTO users VALUES (NULL,'$message','$email')";
+       $simpan = $db->query($data);
+       if($simpan) {
+         echo "<div align='center'>Feedback Sukses, Terima Kasih!</div>";
+         ?> <META HTTP-EQUIV="refresh" CONTENT="3;URL=Home.php"> <?php
+       } else {
+         echo "<div align='center'>Feedback Gagal!</div>";
+         ?> <META HTTP-EQUIV="refresh" CONTENT="3;URL=Feedback.php"> <?php
+       }
+     }
     }
-}
-?>
+ ?>
