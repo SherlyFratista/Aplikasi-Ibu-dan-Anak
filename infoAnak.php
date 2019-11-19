f2ba1c28cae97a560e2d1d2f464f0919e8b7d343
Khanza <alkhanza@icloud.com>
2019-11-19 20:18:09 +0700

    menambahkan artikel dan merapihkan css

--- infoAnak.php
+++ infoAnak.php
@@ -49,7 +49,7 @@ $artikel_TumbuhKembangAnak = query("SELECT * FROM artikel_TumbuhKembangAnak");
 	<div class="card mb-3" style="max-width: 1300px;">
   <div class="row no-gutters">
     <div class="col-md-4">
-      <img src="img/baby.jpg" class="card-img" alt="baby">
+      <img src="img/kenali-nutrisi-otak-yang-mendukung-kehebatan-anak_1166_shutterstock_568555759__1568268610_52695.jpg" class="card-img" alt="baby">
     </div>
     <div class="col-md-8">
       <div class="card-body">
