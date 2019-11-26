d0a5af3105978e2a4a9fd9910e502e051e37400b
SherlyFratista <48596801+SherlyFratista@users.noreply.github.com>
2019-11-26 15:29:09 +0700

    lastProgressFeedback

--- KirimFeedback.php
+++ KirimFeedback.php
@@ -1,23 +1,18 @@
 <?php
-   require_once("koneksi.php");
-   if(isset($_POST['submit'])){
-   $email = $_POST['email'];
-   $message = $_POST['message'];
-   $sql = "SELECT * FROM users WHERE email = '$email'";
-   $query = $db->query($sql);
-   if(!$email || !$message) {
-    echo "<div align='center'>Masih ada data yang kosong! ";
-    ?> <META HTTP-EQUIV="refresh" CONTENT="3;URL=Feedback.php"> <?php
-  } else {
-       $data = "INSERT INTO users VALUES (NULL,'$message','$email')";
-       $simpan = $db->query($data);
-       if($simpan) {
-         echo "<div align='center'>Feedback Sukses, Terima Kasih!</div>";
-         ?> <META HTTP-EQUIV="refresh" CONTENT="3;URL=Home.php"> <?php
-       } else {
-         echo "<div align='center'>Feedback Gagal!</div>";
-         ?> <META HTTP-EQUIV="refresh" CONTENT="3;URL=Feedback.php"> <?php
-       }
-     }
-    }
+session_start();
+require_once("koneksi.php");
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $username = $_SESSION['username'];
+  $data = "INSERT INTO feedback VALUES (NULL,'$message', '$email','$username',now())";
+  $simpan = $db->query($data);
+  if ($simpan) {
+    echo "<div align='center'>Feedback Sukses, Terima Kasih!</div>";
+    ?>
+    <META HTTP-EQUIV="refresh" CONTENT="3;URL=Home.php"> <?php
+ } else {
+  echo "<div align='center'>Feedback Gagal!</div>";
  ?>
+    <META HTTP-EQUIV="refresh" CONTENT="3;URL=Feedback.php"> <?php
+ }
+                                                              ?>
